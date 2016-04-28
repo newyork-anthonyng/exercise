4e9ed89a51fd5daf05c682487a2a2a283d10d9cd
Anthony Ng <newyork.anthonyng@gmail.com>
2016-04-28 17:52:02 -0400

    Add getPositionInfo() with passing tests

--- public/app.js
+++ public/app.js
@@ -13,7 +13,32 @@ var DecipherParser = (function() {
     });
   }
 
+  let getPositionInfo = function(rawData) {
+    let myPositions = [];
+
+    for(let i = 0; i < rawData.length; i++) {
+      const currentData = rawData[i];
+      const gains = currentData['gains'];
+      const currentPosition = {};
+
+      currentPosition['unrealized'] = gains['unrealized_longterm'] + gains['unrealized_shortterm'];
+      currentPosition['realized'] = gains['realized_longterm'] + gains['realized_shortterm'];
+      currentPosition['shortterm'] = gains['unrealized_shortterm'] + gains['realized_shortterm'];
+      currentPosition['longterm'] = gains['unrealized_longterm'] + gains['realized_longterm'];
+      currentPosition['dividends'] = currentData['dividends']['total'];
+      currentPosition['shares'] = currentData['shares'];
+      currentPosition['value'] = currentData['value'];
+      currentPosition['name'] = currentData['security']['name'];
+      currentPosition['ticker'] = currentData['security']['ticker'];
+
+      myPositions.push(currentPosition);
+    }
+
+    return myPositions;
+  }
+
   return {
-    requestInfo: requestInfo
+    requestInfo: requestInfo,
+    getPositionInfo: getPositionInfo
   };
 })();
--- test/spec.js
+++ test/spec.js
@@ -26,6 +26,55 @@ describe('DecipherParser', function() {
 
       expect(callback.calls.count()).toEqual(1);
     });
+  });
+
+  describe('#getPositionInfo', function() {
+
+    it('should return AcctName, Security, and Value', function() {
+      var fakeData = [
+        {
+          gains: { unrealized_longterm: 123, unrealized_shortterm: 234, realized_shortterm: 345, realized_longterm: 456 },
+          dividends: { total: 22 },
+          shares: 123,
+          value: 2985,
+          security: { ticker: 'AAPL', name: 'Apple' }
+        },
+        {
+          gains: { unrealized_longterm: 55, unrealized_shortterm: 66, realized_shortterm: 77, realized_longterm: 88 },
+          dividends: { total: 22 },
+          shares: 234,
+          value: 1984,
+          security: { ticker: 'FB', name: 'Facebook' }
+        },
+      ];
+      var positionInfo = DecipherParser.getPositionInfo(fakeData);
+      var aapl = {
+        unrealized: 357,
+        realized: 801,
+        shortterm: 579,
+        longterm: 579,
+        dividends: 22,
+        shares: 123,
+        value: 2985,
+        name: 'Apple',
+        ticker: 'AAPL'
+      };
+      var fb = {
+        unrealized: 121,
+        realized: 165,
+        shortterm: 143,
+        longterm: 143,
+        dividends: 22,
+        shares: 234,
+        value: 1984,
+        name: 'Facebook',
+        ticker: 'FB'
+      };
+
+      expect(positionInfo.length).toEqual(2);
+      expect(positionInfo[0]).toEqual(aapl);
+      expect(positionInfo[1]).toEqual(fb);
+    });
 
   });
 
