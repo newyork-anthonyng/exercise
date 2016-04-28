3248d95ed6a424d9c5851d793002bfc4d4a83ab2
Anthony Ng <newyork.anthonyng@gmail.com>
2016-04-28 18:21:40 -0400

    Refactor getPositionInfo()

--- public/app.js
+++ public/app.js
@@ -17,24 +17,28 @@ var DecipherParser = (function() {
     let myPositions = [];
 
     for(let i = 0; i < rawData.length; i++) {
-      const currentData = rawData[i];
-      const gains = currentData['gains'];
-      const currentPosition = {};
-
-      currentPosition['unrealized'] = gains['unrealized_longterm'] + gains['unrealized_shortterm'];
-      currentPosition['realized'] = gains['realized_longterm'] + gains['realized_shortterm'];
-      currentPosition['shortterm'] = gains['unrealized_shortterm'] + gains['realized_shortterm'];
-      currentPosition['longterm'] = gains['unrealized_longterm'] + gains['realized_longterm'];
-      currentPosition['dividends'] = currentData['dividends']['total'];
-      currentPosition['shares'] = currentData['shares'];
-      currentPosition['value'] = currentData['value'];
-      currentPosition['name'] = currentData['security']['name'];
-      currentPosition['ticker'] = currentData['security']['ticker'];
-
+      const currentPosition = parsePositionInfo(rawData[i]);
       myPositions.push(currentPosition);
     }
 
     return myPositions;
+  }
+
+  function parsePositionInfo(positionInfo) {
+    const myPosition = {};
+    const gains = positionInfo['gains'];
+
+    myPosition['unrealized'] = gains['unrealized_longterm'] + gains['unrealized_shortterm'];
+    myPosition['realized'] = gains['realized_longterm'] + gains['realized_shortterm'];
+    myPosition['shortterm'] = gains['unrealized_shortterm'] + gains['realized_shortterm'];
+    myPosition['longterm'] = gains['unrealized_longterm'] + gains['realized_longterm'];
+    myPosition['dividends'] = positionInfo['dividends']['total'];
+    myPosition['shares'] = positionInfo['shares'];
+    myPosition['value'] = positionInfo['value'];
+    myPosition['name'] = positionInfo['security']['name'];
+    myPosition['ticker'] = positionInfo['security']['ticker'];
+
+    return myPosition;
   }
 
   return {
